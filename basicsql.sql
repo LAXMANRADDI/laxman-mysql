b79199b758ce2122944b6d0da909cece6fec407c
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-09-23 23:35:47 +0530

    Create basicsql.sql

---
+++ basicsql.sql
@@ -0,0 +1,97 @@
+schema --> collection of n number of databases 
+dbms--> data base management system
+rdbms --> relational database management system
+se --> storage engine // compiler
+ --------------------------------------------------------------------------------- since mysql did not have gui  we r using mysql workbench for practicing ---------------------------------------------
+ available data types in my sql 
+•int
+ •smallint
+ •bigint
+ •tinyint
+ •mediumint
+ •decimal(p,d)
+ •float
+ •double
+ •char
+ •varchar
+ •binary
+ •varbinary
+ •datetime
+ •date
+ •time
+ •timestamp
+ •year
+ • bit
+ • real
+ • numeric(p,d)
+ • xml
+ • text
+ • tinytext
+ • mediumtext
+ • longtext
+ • blob
+
+ but varchar , int , date , char  are used in major cases 
+
+
+DATA TYPES                         SIZE IN BYTES
+
+ • tinyint                  ----------->  1 byte
+ • smallint                   --------------> 2 byte
+ • mediumint              ------------------> 3 byte
+ • int                        ----------------> 4 byte
+ • bigint              -----------------------> 8 byte 
+ • decimal(p,d)          ----------------->  can store upto65 digits 30 can be after decimal point
+ • bit                        -------->  1 bit  ( can store either 0 or1)           // note:  usually used to store true or false
+
+ • char(x)      --------------->  can store max 255 charector 
+ • varchar(x)         ------------> 16383 charectors 
+ • tinytext    -------------------> 256 bytes 
+ • text          ----------------> 65535 bytes    (~64 kb )
+ • mediumtext         --------------------> ~16mb          //16777215 bytes 
+ • longtext            -------------------->      4294967295 bytes (~ 4gb)
+
+
+
+
+ char vs varchar 
+ >> char allocates memory while declaring  ,
+ >> varchar uses dynamic memory allocation as user gives input it will allocates memory 
+
+----------------------------------------------------------------------  
+  dml  --------- >data modification lang
+  dql  -------- >>data query lang
+  ddl ---------> data definition lang 
+  dcl --------->>  data contrlo lang 
+  tcl  ------------- >>  transaction control lang 
+
+ 
+
+simple table creation and table operations 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
