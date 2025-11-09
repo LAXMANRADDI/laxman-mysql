c5f7911c023c188f83eac2d1149f97a1bc0cc8e3
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-09 23:26:26 +0530

    Update tableop.sql

--- tableop.sql
+++ tableop.sql
@@ -1,7 +1,13 @@
-1] delete command with where conndition :
+1] delete command with where clause :
 syntax:
 DELETE FROM table_name WHERE condition;
 ex: to delete records of student with stdid = 5
 DELETE FROM Students
 WHERE StudentID = 5;
 
+2] delete command without where clause:
+syntax :
+DELETE FROM table_name;
+ex:
+DELETE FROM Students;
+imp:[Deletes all records (table becomes empty), but table still exists.]
