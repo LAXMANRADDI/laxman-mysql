1e3f7d54dedfcec30f8929d763a817834b311c09
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-09 23:29:18 +0530

    Update tableop.sql

--- tableop.sql
+++ tableop.sql
@@ -1,11 +1,11 @@
-1] delete command with where clause :
+1.1] delete command with where clause :
 syntax:
 DELETE FROM table_name WHERE condition;
 ex: to delete records of student with stdid = 5
 DELETE FROM Students
 WHERE StudentID = 5;
 
-2] delete command without where clause:
+1.2] delete command without where clause:
 syntax :
 DELETE FROM table_name;
 ex:
@@ -16,3 +16,11 @@ imp:[Deletes all records (table becomes empty), but table still exists.]
 Applications
 Remove specific old or invalid data (with WHERE)
 Empty table but keep structure for reuse (without WHERE)
+
+
+2] drop command:can be used to drop table or whole database 
+  syntax:
+DROP TABLE table_name;
+ex:
+  DROP TABLE Students;
+Deletes table Students completely (data + structure).
