0afa8d14f024999d68dfe2fff59c4a29d55627b6
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-29 00:00:10 +0530

    Create sql joins.sql

---
+++ sql joins.sql
@@ -0,0 +1,19 @@
+
+A JOIN is used to combine rows from two or more tables based on a related column between them.
+
+Example structure:
+
+Table: students
+
+id	name	dept_id
+
+1	Rahul	10
+2	Meena	20
+
+
+Table: departments
+
+dept_id	dept_name
+
+10	CSE
+20	ECE
