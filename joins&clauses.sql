7ad98452c8fd05099a47b0b74336f7661af53f6f
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-12-08 22:33:33 +0530

    Update and rename sql joins.sql to  joins&clauses.sql

--- joins&clauses.sql
+++ joins&clauses.sql
@@ -1,38 +1,29 @@
-
 A JOIN is used to combine rows from two or more tables based on a related column between them.
-
 Example structure:
-
 Table: students
-
 id	name	dept_id
 1	 Rahul 	10
 2 	Meena	20
 
 
 Table: departments
-
 dept_id	dept_name
 10     	CSE
 20	     ECE
 
 
 1]INNER JOIN:
-
 Returns rows only when matching data exists in both tables.
-
-SELECT s.name, d.dept_name
+SeLECT s.name, d.dept_name
 FROM students s
 INNER JOIN departments d
 ON s.dept_id = d.dept_id;
-
 -->Only matching dept_ids will appear.
 
 
 2]LEFT JOIN (LEFT OUTER JOIN)
 
 Returns all rows from left table, plus matching rows from right.
-
 SELECT s.name, d.dept_name
 FROM students s
 LEFT JOIN departments d
@@ -42,9 +33,7 @@ ON s.dept_id = d.dept_id;
 
 
 3] RIGHT JOIN (RIGHT OUTER JOIN)
-
 Opposite of left join.
-
 SELECT s.name, d.dept_name
 FROM students s
 RIGHT JOIN departments d
@@ -55,17 +44,14 @@ ON s.dept_id = d.dept_id;
 
 ---
 4] FULL OUTER JOIN
-
 *)Not directly supported in MySQL.
 We use UNION:
-
 SELECT s.*, d.*
 FROM students s
 LEFT JOIN departments d
 ON s.dept_id = d.dept_id
 
 UNION
-
 SELECT s.*, d.*
 FROM students s
 RIGHT JOIN departments d
@@ -74,20 +60,24 @@ ON s.dept_id = d.dept_id;
 
 
 5] CROSS JOIN
-
 Returns all combinations (cartesian product).
-
 SELECT *
 FROM students
 CROSS JOIN departments;
-
 If 10 students × 3 depts → 30 rows.
-
+  
 6]SELF JOIN
-
 A table joined with itself.
-
 SELECT e1.name AS employee, e2.name AS manager
 FROM employees e1
 JOIN employees e2
 ON e1.manager_id = e2.emp_id;
+
+
+MASTER TABLE FOR ALL FOLLOWING CLAUSES:
+emp_id   name  department  salary  join_year 
+ 1      Rahul  IT          50000   2022      
+ 2      Meena  HR          60000   2023      
+ 3      Arjun  IT          70000   2021      
+ 4      Priya  Sales       45000   2024      
+ 5      Karan  HR          55000   2022      
